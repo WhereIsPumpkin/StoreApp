a845f1039bf34d089319ac9cea3c18ddf2aefe08
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2023-12-17 17:35:56 +0400

    Initial Commit

---
+++ StoreApp/App/StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreAppApp.swift
+//  StoreApp
+//
+//  Created by Saba Gogrichiani on 17.12.23.
+//
+
+import SwiftUI
+
+@main
+struct StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}  
